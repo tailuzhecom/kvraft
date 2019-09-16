661a0083db106d96fbf680aa2d0a112796123bab
tailuzhecom <tailuzhecom@foxmail.com>
2019-09-16 23:40:12 +0800

    lab 1 part 3

--- src/mapreduce/schedule.go
+++ src/mapreduce/schedule.go
@@ -1,6 +1,9 @@
 package mapreduce
 
-import "fmt"
+import (
+	"fmt"
+	"sync"
+)
 
 //
 // schedule() starts and waits for all tasks in the given phase (mapPhase
@@ -18,6 +21,7 @@ func schedule(jobName string, mapFiles []string, nReduce int, phase jobPhase, re
 	case mapPhase:
 		ntasks = len(mapFiles)
 		n_other = nReduce
+
 	case reducePhase:
 		ntasks = nReduce
 		n_other = len(mapFiles)
@@ -30,5 +34,26 @@ func schedule(jobName string, mapFiles []string, nReduce int, phase jobPhase, re
 	//
 	// Your code here (Part III, Part IV).
 	//
+	var wg sync.WaitGroup
+	wg.Add(ntasks)
+	for i := 0; i < ntasks; i++ {
+		worker_addr := <- registerChan
+		func_arg := DoTaskArgs{jobName, mapFiles[i], phase, i, n_other}
+
+		go func(worker_addr string, func_arg DoTaskArgs) {
+			defer wg.Done()
+			for {
+				ret := call(worker_addr, "Worker.DoTask", func_arg, nil)
+				if ret {  // 成功返回，否则一直运行到成功为止
+					go func(registerChan chan string, worker_addr string) {
+						registerChan <- worker_addr // worker变为可用状态
+					}(registerChan, worker_addr)
+					break
+				}
+			}
+
+		}(worker_addr, func_arg)
+	}
+	wg.Wait()
 	fmt.Printf("Schedule: %v done\n", phase)
 }
