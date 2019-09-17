00ae6481a7fba7422eddfd50f356f2297483dd9e
tailuzhecom <tailuzhecom@foxmail.com>
2019-09-17 19:29:39 +0800

    lab 1 part 4

--- src/mapreduce/schedule.go
+++ src/mapreduce/schedule.go
@@ -35,25 +35,53 @@ func schedule(jobName string, mapFiles []string, nReduce int, phase jobPhase, re
 	// Your code here (Part III, Part IV).
 	//
 	var wg sync.WaitGroup
+	//wg.Add(ntasks)
+	//for i := 0; i < ntasks; i++ {
+	//	worker_addr := <- registerChan
+	//	func_arg := DoTaskArgs{jobName, mapFiles[i], phase, i, n_other}
+	//
+	//	go func(worker_addr string, func_arg DoTaskArgs) {
+	//		defer wg.Done()
+	//		for {
+	//			ret := call(worker_addr, "Worker.DoTask", func_arg, nil)
+	//			if ret {  // 成功返回，否则一直运行到成功为止
+	//				go func(registerChan chan string, worker_addr string) {  // 这里要使用goroutine
+	//					registerChan <- worker_addr // worker变为可用状态
+	//				}(registerChan, worker_addr)
+	//				break
+	//			}
+	//		}
+	//
+	//	}(worker_addr, func_arg)
+	//}
+
 	wg.Add(ntasks)
-	for i := 0; i < ntasks; i++ {
-		worker_addr := <- registerChan
-		func_arg := DoTaskArgs{jobName, mapFiles[i], phase, i, n_other}
-
-		go func(worker_addr string, func_arg DoTaskArgs) {
-			defer wg.Done()
-			for {
-				ret := call(worker_addr, "Worker.DoTask", func_arg, nil)
-				if ret {  // 成功返回，否则一直运行到成功为止
-					go func(registerChan chan string, worker_addr string) {
-						registerChan <- worker_addr // worker变为可用状态
-					}(registerChan, worker_addr)
-					break
-				}
-			}
-
-		}(worker_addr, func_arg)
+	taskChan := make(chan int, ntasks)
+	// 把任务放入任务队列
+	for i:=0;i<ntasks;i++  {
+		taskChan <- i
 	}
+
+	// 从worker队列中获取可用的worker，并在一个gorountine中一直占用这个worker
+	// worker会监测是否有任务需要完成，每取到一个task，就会去执行一次，成功告知WaitGroup，
+	// 失败将任务返回任务队列
+	go func() {
+		for {
+			worker_addr := <- registerChan
+			go func(worker_addr string) {
+				for {
+					i := <- taskChan
+					if call(worker_addr,"Worker.DoTask", &DoTaskArgs{jobName,
+						mapFiles[i],phase,i,n_other},nil){
+						wg.Done()
+					} else{
+						taskChan <- i
+					}
+				}
+			}(worker_addr)
+		}
+	}()
+
 	wg.Wait()
 	fmt.Printf("Schedule: %v done\n", phase)
 }
